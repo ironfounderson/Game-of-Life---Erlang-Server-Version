abb16303ecaff4ed4a464ec8b3515093b7d8f4bb
Robert Höglund <roberthoglund@gmail.com>
2010-12-05 13:58:01 +0100

    Getting started

--- gol.erl
+++ gol.erl
@@ -1,4 +1,15 @@
 -module(gol).
 -compile(export_all).
 
+%% initial state is represented by an array of strings where 0 = empty cell
+%% and 1 = occupied cell
+
+start_blinker() ->
+    start(["00000", "00000", "01110", "00000", "00000"]).
+
+start([IS | InitialStates]) ->
+    io:format("Columns = ~p, Rows = ~p~n", 
+              [length(IS), 
+               length([IS|InitialStates])]),
+    ok.
 
